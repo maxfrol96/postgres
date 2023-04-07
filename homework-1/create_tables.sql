084f4c78e57e24106e6a08fbcc6b0b9813d603a7
maxfrol96 <maxfrol96@mail.ru>
2023-04-07 10:59:23 +0300

    dz1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,36 @@
--- SQL-команды для создания таблиц
+CREATE TABLE IF NOT EXISTS public.customers
+(
+    customer_id character varying(5) COLLATE pg_catalog."default" NOT NULL,
+    company_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    contact_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT customers_pkey PRIMARY KEY (customer_id)
+)
+
+CREATE TABLE IF NOT EXISTS public.employees
+(
+    employee_id integer NOT NULL DEFAULT nextval('employees_employee_id_seq'::regclass),
+    first_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(40) COLLATE pg_catalog."default" NOT NULL,
+    birth_date date NOT NULL,
+    notes text COLLATE pg_catalog."default",
+    CONSTRAINT employees_pkey PRIMARY KEY (employee_id)
+)
+
+CREATE TABLE IF NOT EXISTS public.orders
+(
+    order_id integer NOT NULL,
+    customer_id character varying(5) COLLATE pg_catalog."default" NOT NULL,
+    employee_id integer NOT NULL,
+    order_date date NOT NULL,
+    ship_city character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT orders_pkey PRIMARY KEY (order_id),
+    CONSTRAINT orders_customer_id_fkey FOREIGN KEY (customer_id)
+        REFERENCES public.customers (customer_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT orders_employee_id_fkey FOREIGN KEY (employee_id)
+        REFERENCES public.employees (employee_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,66 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+
+customers = []
+employees = []
+orders = []
+password = os.getenv('PSQL_pass')
+
+with open('C://Users//KO77-31//Desktop//skypro//SQL//postgres-homeworks//homework-1//north_data//employees_data.csv',
+          'r') as file:
+    for row in file:
+        res = row.split('",')
+        red_res =[]
+        for item in res:
+            item = item.replace('"', '')
+            item = item.replace('\n', '')
+            item = item.replace("'", '')
+            red_res.append(item)
+        employees.append(red_res)
+    employees = employees[1:len(employees)]
+
+print(employees)
+
+with open('C://Users//KO77-31//Desktop//skypro//SQL//postgres-homeworks//homework-1//north_data//customers_data.csv',
+          'r') as file:
+    for row in file:
+        res = row.split(',')
+        red_res =[]
+        for item in res:
+            item = item.replace('"', '')
+            item = item.replace('\n', '')
+            item = item.replace("'", '')
+            red_res.append(item)
+        customers.append(red_res)
+    customers = customers[1:len(customers)]
+
+with open('C://Users//KO77-31//Desktop//skypro//SQL//postgres-homeworks//homework-1//north_data//orders_data.csv',
+          'r') as file:
+    for row in file:
+        res = row.split(',')
+        red_res =[]
+        for item in res:
+            item = item.replace('"', '')
+            item = item.replace('\n', '')
+            item = item.replace("'", '')
+            red_res.append(item)
+        orders.append(red_res)
+    orders = orders[1:len(orders)]
+
+with psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password=password
+) as conn:
+    with conn.cursor() as cur:
+        for item in customers:
+            cur.execute(f"INSERT INTO customers values ('{item[0]}', '{item[1]}', '{item[2]}')")
+        id = 1
+        for item in employees:
+            cur.execute(f"INSERT INTO employees values ({id}, '{item[0]}', '{item[1]}', '{item[2]}', '{item[3]}', '{item[4]}')")
+            id += 1
+        for item in orders:
+            cur.execute(f"INSERT INTO orders values ('{item[0]}', '{item[1]}', '{item[2]}', '{item[3]}', '{item[4]}')")
+
